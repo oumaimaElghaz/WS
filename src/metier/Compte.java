820367f802c25e6657c2c2115885c99e7fe32a9c
Oumaima EL GHAZOUANI <oumaima.elghazouani.stage@worldaudit.ma>
2021-10-26 01:46:12 +0200

    first commit

---
+++ src/metier/Compte.java
@@ -0,0 +1,48 @@
+package metier;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.Date;
+@XmlRootElement(name = "compte")//c'est pour prendre compte comme element xml
+@XmlAccessorType(XmlAccessType.FIELD)//c'est pour prendre xmltransient uniquement sur les attribut et non pas sur les getters et setters
+public class Compte {
+    private Long code;
+    private double solde;
+    @XmlTransient//pour ignorer la date de creation
+    private Date dateCreation;
+
+    public Compte(Long code, double solde, Date dateCreation) {
+        this.code = code;
+        this.solde = solde;
+        this.dateCreation = dateCreation;
+    }
+
+    public Compte() {
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+}
